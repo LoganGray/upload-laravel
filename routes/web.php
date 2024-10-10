8d2abf3d1480da5cc3487b2d05e6316cef671be9
Logan Gray <logan@intensetech.com>
2024-10-10 16:07:15 -0700

    feat: Add file upload functionality

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-
-
-    
-});
-<?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileUploadController;
+
+
 
 Route::get('/', [FileUploadController::class, 'index']);
 Route::post('/upload', [FileUploadController::class, 'store']);
+
